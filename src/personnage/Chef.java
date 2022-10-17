cf906a35389c66cde937f99d61f4fc1eb5133380
SilasTeuliere <silas.teuliere@gmail.com>
2022-10-17 20:21:18 +0200

    TP2

--- src/personnage/Chef.java
+++ src/personnage/Chef.java
@@ -1,16 +1,17 @@
 package personnage;
 
+import lieu.Village;
+
 public class Chef {
 	private String nom;
 	private int force;
 	//private int effetPotion = 1;
-	//private Village village;
-	//, Village village
-	public Chef(String nom, int force) {
+	private Village village;
+	public Chef(String nom, int force, Village village) {
 		this.nom = nom;
 		this.force = force;
 		//this.effetPotion = effetPotion;
-		//this.village = village;
+		this.village = village;
 	}
 	public String getNom() {
 		return nom;
@@ -18,9 +19,9 @@ public class Chef {
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "« " + texte + "»");
 	}
-	//+ " du village " + village.getNom()
+	//
 	private String prendreParole() {
-		return "Le chef " + nom  + " : ";
+		return "Le chef " + nom  + " du village " + village.getNom() +" : ";
 	}
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de " +
--- src/personnage/Romain.java
+++ src/personnage/Romain.java
@@ -3,32 +3,82 @@ package personnage;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement =0;
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert force > 0 ;
 	}
+	
 	public String getNom() {
 		return nom;
 	}
+	
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "« " + texte + "»");
 	}
+	
 	public String prendreParole() {
 		return "Le romain " + nom + " : ";
 	}
+	
 	public void recevoirCoup(int forceCoup) {
+		assert force > 0 ;
+		int forceNonDim = force;
 		force -= forceCoup;
 		if (force > 0) {
 				parler("Aïe");
 		} else {
 			parler("J'abandonne...");
 		}
+		assert forceNonDim > force ;
 	}
-	/*public static void main(String[] args) {
-		Romain cesar = new Romain("Cesar", 7);
-		cesar.prendreParole();
-		cesar.parler("Ave");
-		cesar.recevoirCoup(5);
-		cesar.recevoirCoup(9);
-	}*/
+	
+	public void sEquiper (Equipement equipement) {
+		   switch(nbEquipement){
+		   
+	       case 2: 
+	           System.out.println("Le soldat " + getNom() + " est déjà bien protégé !");
+	           break;
+	   
+	       case 1:
+	    	   if(equipements[0] == equipement || equipements[1] == equipement) {
+	    		   System.out.println("Le soldat " + getNom() + " possède déjà un " + equipement.toString() + " !");
+	    	   }
+	    	   else {
+	    		   if(equipements[0].toString() == "") {
+	    			   equipements[0] = equipement;
+	    			   System.out.println("Le soldat " + getNom() + " s'équipe avec un " + equipement.toString() + " !");
+	    			   nbEquipement++;
+	    		   }
+	    		   else if (equipements[1] == null){
+	    			   equipements[1] = equipement;
+	    			   System.out.println("Le soldat " + getNom() + " s'équipe avec un " + equipement.toString() + " !");
+	    			   nbEquipement++;
+	    		   }
+	    	   }
+	           break;
+	   
+	       default:
+	           equipements[0] = equipement; 
+	           System.out.println("Le soldat " + getNom() + " s'équipe avec un " + equipement.toString() + " !");
+	           nbEquipement++;
+	           break;
+		   }
+	}
+	
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 6);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+		
+		minus.prendreParole();
+		minus.parler("Ave");
+		minus.recevoirCoup(5);
+		minus.recevoirCoup(9);
+		System.out.println(Equipement.CASQUE + " " + Equipement.BOUCLIER);
+	}
 }
